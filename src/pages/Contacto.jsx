8c4376867f2e0215c2107ee8cb098fa3b0729d1c
allamemc <allamemc@gmail.com>
2024-02-27 22:58:00 +0100

    Update Contacto page with new form

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -1,7 +1,47 @@
 export default function Contacto() {
 	return (
-		<div>
-			<h1>Contacto</h1>
+		<div className='hero min-h-screen bg-base-200'>
+			<div className='hero-content flex-col lg:flex-row-reverse w-full'>
+				<div className='card shrink-0 w-full max-w-96 shadow-2xl bg-base-100'>
+					<form className='card-body'>
+						<h1 className='text-3xl font-bold text-center'>Te escuchamos</h1>
+						<div className='form-control'>
+							<label className='label'>
+								<span className='label-text'>Nombre</span>
+							</label>
+							<input
+								type='text'
+								placeholder='Juan'
+								className='input input-bordered'
+								required
+							/>
+						</div>
+						<div className='form-control'>
+							<label className='label'>
+								<span className='label-text'>Email</span>
+							</label>
+							<input
+								type='email'
+								placeholder='juan@gmail.com'
+								className='input input-bordered'
+								required
+							/>
+						</div>
+						<div className='form-control'>
+							<label className='label'>
+								<span className='label-text'>Tu mensaje</span>
+							</label>
+							<textarea
+								className='textarea textarea-bordered'
+								placeholder='...'></textarea>
+						</div>
+
+						<div className='form-control mt-6'>
+							<button className='btn btn-primary'>Enviar</button>
+						</div>
+					</form>
+				</div>
+			</div>
 		</div>
 	)
 }
