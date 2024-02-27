230d8c0b0ed9084407e0b9d4278d0cbe711e5a44
allamemc <allamemc@gmail.com>
2024-02-27 23:22:57 +0100

    Add responsive layout and trainers information to About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,65 @@
 export default function About() {
 	return (
-		<div>
-			<h1>About</h1>
+		<div className='hero  h-screen'>
+			<div className='flex flex-col mt-12 text-center hero-content'>
+				<div className='max-w-ld'>
+					<h1 className='text-5xl font-bold '>Nuestros Entrenadores</h1>
+					<p className='py-6 '>
+						Nuestros entrenadores Pokémon son expertos dedicados y apasionados.
+					</p>
+				</div>
+				<div className='flex flex-col self-center justify-center w-full h-auto p-16 rounded-none animate-none gap-14  flex-clases'>
+					<div className='flex self-center items-center justify-center w-full gap-8 flex-clases'>
+						<div className='w-full rounded shadow-xl card card-compact bg-base-100'>
+							<figure>
+								<div
+									className='profesor shifu w-1/2'
+									style={{
+										height: '300px',
+									}}></div>
+							</figure>
+							<div className='card-body'>
+								<h2 className='card-title'>Profesor Oak</h2>
+								<p className='text-left'>
+									Con un corazón noble, protege a los Pokémon con compasión y
+									cuidado, mostrando que la verdadera grandeza radica en el amor
+									y el respeto por todas las criaturas.
+								</p>
+							</div>
+						</div>
+						<div className='w-full rounded shadow-xl card card-compact bg-base-100'>
+							<figure>
+								<div
+									className='profesor rockie'
+									style={{ height: '300px' }}></div>
+							</figure>
+							<div className='card-body'>
+								<h2 className='card-title'>Ash Ketchum</h2>
+								<p className='text-left'>
+									Con su carisma innato, lidera con sabiduría y valentía,
+									inspirando a otros a alcanzar sus sueños Pokémon con pasión y
+									determinación.
+								</p>
+							</div>
+						</div>
+						<div className='w-full rounded shadow-xl card card-compact bg-base-100'>
+							<figure>
+								<div
+									className='profesor thai'
+									style={{ height: '300px' }}></div>
+							</figure>
+							<div className='card-body'>
+								<h2 className='card-title'>Misty</h2>
+								<p className='text-left'>
+									Experto en estrategia, desafía a sus oponentes con
+									inteligencia y astucia, demostrando que la fuerza mental es
+									tan crucial como la física en el mundo Pokémon.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	)
 }
--- src/pages/Servicios.jsx
+++ src/pages/Servicios.jsx
@@ -1,7 +1,138 @@
 export default function Servicios() {
 	return (
-		<div>
-			<h1>Servicios</h1>
+		<div className='hero min-h-screen bg-base-200'>
+			<div className='hero-content flex-col lg:flex-row-reverse w-full'>
+				<div className='card shrink-0 w-full max-w-3xl shadow-2xl bg-base-100'>
+					<div className='card-body'>
+						<h1 className='text-3xl font-bold text-center mb-4'>
+							Servicios Pokémon
+						</h1>
+						<p className='text-lg mb-4'>
+							Descubre los increíbles servicios que ofrecemos para entrenadores
+							Pokémon
+						</p>
+						<div className='collapse collapse-arrow bg-base-200'>
+							<input type='radio' name='my-accordion-2' defaultChecked />
+							<div className='collapse-title text-xl font-medium'>
+								Tiendas Pokemon
+							</div>
+							<div className='collapse-content'>
+								<p>
+									Explora nuestras tiendas Pokémon, donde encontrarás una amplia
+									variedad de productos para entrenadores Pokémon, incluyendo:
+								</p>
+								<ul className='list-disc ml-6'>
+									<li>
+										Todo tipo de Poké Balls para capturar Pokémon salvajes.
+									</li>
+									<li>
+										Objetos especiales como pociones, superpociones y revivir
+										para mantener a tus Pokémon en plena forma.
+									</li>
+									<li>
+										Ropa y accesorios con los diseños más geniales de Pokémon.
+									</li>
+									<li>
+										Figuras coleccionables y tarjetas de entrenador para
+										expandir tu colección.
+									</li>
+								</ul>
+								<br />
+								<div className='flex justify-center'>
+									<img
+										src='https://media.timeout.com/images/105684311/image.jpg'
+										className='max-w-full max-h-full rounded'
+									/>
+								</div>
+								<div className='mt-4 flex justify-center'>
+									<button className='btn btn-primary'>Saber más</button>
+								</div>
+							</div>
+						</div>
+
+						<div className='collapse collapse-arrow bg-base-200'>
+							<input type='radio' name='my-accordion-2' />
+							<div className='collapse-title text-xl font-medium'>
+								Centros de Salud Pokémon
+							</div>
+							<div className='collapse-content'>
+								<p>
+									Visita nuestros Centros de Salud Pokémon para sanar y
+									revitalizar a tus compañeros de batalla después de una intensa
+									batalla. Ofrecemos:
+								</p>
+								<ul className='list-disc ml-6'>
+									<li>
+										Atención médica gratuita y de calidad para todos tus
+										Pokémon.
+									</li>
+									<li>
+										Personal amable y experimentado para cuidar de tus
+										compañeros Pokémon.
+									</li>
+									<li>
+										Una estancia confortable mientras tus Pokémon se recuperan.
+									</li>
+									<li>
+										Servicios adicionales como entrenamiento y asesoramiento
+										para entrenadores novatos.
+									</li>
+								</ul>
+								<br />
+								<div className='flex justify-center'>
+									<img
+										src='https://pm1.aminoapps.com/6295/87f9ca5c79af4525f4d669b54c40452739bcfb6e_00.jpg'
+										className='max-w-full max-h-full rounded'
+									/>
+								</div>
+								<div className='mt-4 flex justify-center'>
+									<button className='btn btn-primary'>Saber más</button>
+								</div>
+							</div>
+						</div>
+
+						<div className='collapse collapse-arrow bg-base-200'>
+							<input type='radio' name='my-accordion-2' />
+							<div className='collapse-title text-xl font-medium'>
+								Gimnasios Pokémon
+							</div>
+							<div className='collapse-content'>
+								<p>
+									Desafía a los líderes de gimnasio Pokémon para probar tu
+									habilidad como entrenador. Nuestros gimnasios ofrecen:
+								</p>
+								<ul className='list-disc ml-6'>
+									<li>
+										Entrenadores expertos que te ayudarán a mejorar tus
+										habilidades de batalla.
+									</li>
+									<li>
+										Pokémon salvajes para entrenar y fortalecer a tu equipo.
+									</li>
+									<li>
+										Equipos y campos de entrenamiento especializados para cada
+										tipo de Pokémon.
+									</li>
+									<li>
+										Recompensas exclusivas para aquellos que superen los
+										desafíos de gimnasio.
+									</li>
+								</ul>
+								<br />
+								<div className='flex justify-center'>
+									<img
+										src='https://images.wikidexcdn.net/mwuploads/wikidex/1/18/latest/20180809183603/EP576_Gimnasio_de_Ciudad_Canal.jpg'
+										className='max-w-full max-h-full rounded'
+									/>
+								</div>
+								<div className='mt-4 flex justify-center'>
+									<button className='btn btn-primary'>Saber más</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	)
 }
